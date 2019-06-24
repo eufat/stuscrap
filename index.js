c5cf824171b4b4abca4b6a00951ea0e9a0a9160e
Eufrat Tsaqib <eutsat@gmail.com>
2019-06-24 19:46:10 +0700

    Scrap with multiple paramters

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const puppeteer = require('puppeteer');
-var fs = require('fs');
+const fs = require('fs');
+const _ = require('lodash');
 
-const URL = "https://forlap.ristekdikti.go.id/mahasiswa/detailsemester/MkI2QTMxRDQtN0NGMi00QkFGLUFGNTgtNTM0MzlFNDU2QUE5/20182";
-const PAGE_NUM = 45;
+const BASE_URL = "https://forlap.ristekdikti.go.id/mahasiswa/detailsemester"
+const DEP_CODE = "MkI2QTMxRDQtN0NGMi00QkFGLUFGNTgtNTM0MzlFNDU2QUE5"
+const YEAR = "2018"
+const TERM = "2"
+const URL = BASE_URL + "/" + DEP_CODE + "/" + YEAR + TERM + "/";
+const PAGE_NUM = 13;
 const PAGE_ROW = 20;
+const OUTPUT_FILE = "scrap.csv";
+const NIM_KEY = "150";
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -12,28 +19,44 @@ const PAGE_ROW = 20;
   datas = []
   let SCRAP_URL = URL
 
-  for (let p = 1; p <= PAGE_NUM + 1; p++) {
+  for (let p = 1; p <= PAGE_NUM; p++) {
     console.log(`Scrapping ${SCRAP_URL}`)
     const page = await browser.newPage();
     await page.goto(SCRAP_URL);
+
+
     const data = await page.evaluate(() => {
         const tds = Array.from(document.querySelectorAll('table tr td'))
-        return tds.map(td => td.innerHTML)
+        return tds.map(td => {
+          if (td.nodeName == "A") {
+            return td.innerHTML
+          } else {
+            return td.innerText
+          }
+        })
     });
 
     datas = datas.concat(data);
-    
+
     SCRAP_URL = URL + (PAGE_ROW * p);
-    // console.log(`Scrapped ${PAGE_ROW * p} / ${PAGE_NUM * PAGE_ROW}`)
+    console.log(`Scrapped ${PAGE_ROW * p} / ${PAGE_NUM * PAGE_ROW}`)
   }
   await browser.close();
 
-  var file = fs.createWriteStream('scrap.txt');
+  let file = fs.createWriteStream(OUTPUT_FILE);
   file.on('error', function(err) { /* error handling */ });
 
-  datas.forEach(function(value) {
+  chunked_datas = _.chunk(datas, 3);
+
+  chunked_datas = chunked_datas.map(val => {
+    if (val[1].includes(NIM_KEY)) {
+      return val.join(', ')
+    }
+  }).filter(value => value != undefined);
+
+  chunked_datas.forEach(function(value) {
     file.write(value + '\n');
   });
 
-  file.end(); 
+  file.end();
 })();
